e0bbf58d9c748beeabcd3d3341969051e003f0f4
Adam Malantonio <malantoa@lafayette.edu>
2016-09-12 15:54:22 -0400

    allow array-like objects

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict'
+var isArrayLike = require('is-array-like')
 
-module.exports = function randomArrayIndex (array) {
-  if (!Array.isArray(array)) {
-    throw TypeError('Expected argument to be an array')
+module.exports = function randomArrayIndex (arr) {
+  if (!isArrayLike(arr)) {
+    throw TypeError('Expected argument to be an array or array-like')
   }
 
-  return Math.floor(Math.random() * array.length)
+  return Math.floor(Math.random() * arr.length)
 }
+
--- test.js
+++ test.js
@@ -11,5 +11,13 @@ for (var i = 0; i < 10000; i++) {
 
 assert.throws(randomIndex)
 assert.throws(function () { randomIndex({}) })
-assert.throws(function () { randomIndex('nope') })
 assert.throws(function () { randomIndex(1234) })
+
+var arrlike = {0: 'one', 1: 'two', 2: 'three', length: 3}
+
+for (var j = 0; j < 10000; j++) {
+	var idx = randomIndex(arrlike)
+	assert.ok(arrlike[idx])
+	assert.ok(idx >= 0)
+	assert.ok(idx < arrlike.length)
+}
